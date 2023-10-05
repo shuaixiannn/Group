21942622cbe75ce55cf7be3d1713145ff3d38fbf
shuaixiannn <140046787+shuaixiannn@users.noreply.github.com>
2023-10-05 09:35:47 +0800

    Add files via upload

---
+++ AdminPage/Reservation/create_process.php
@@ -0,0 +1,24 @@
+<!--Christian-->
+<?php
+// Include your database connection code here
+include '../db.php';
+
+// Retrieve form data
+$name = $_POST['name'];
+$contact = $_POST['contact'];
+$reservation_date = $_POST['reservation_date'];
+$reservation_time = $_POST['reservation_time'];
+$table_id = $_POST['table_id'];
+
+// Insert the new reservation into the database
+$sql = "INSERT INTO reservations (name, contact, reservation_date, reservation_time, table_id) 
+        VALUES ('$name', '$contact', '$reservation_date', '$reservation_time', '$table_id')";
+
+if ($conn->query($sql) === TRUE) {
+    header("Location: reservation.php");
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
